10305f815ad2df98f3d8de67618060a7e2b25235
serine bagdasaryan <serinebagdasaryan5@gmail.com>
2018-08-05 12:59:27 +0400

    commit

---
+++ routing/app/classes/classes_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('myApp.classes module', function() {
+
+  beforeEach(module('myApp.classes'));
+
+  describe('classes controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var classesCtrl = $controller('ClassesCtrl');
+      expect(classesCtrl).toBeDefined();
+    }));
+
+  });
+});
